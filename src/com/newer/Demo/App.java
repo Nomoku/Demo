ea43f5b328b9d800301727fce2333149a54a6381
Nomoku <13212626570@163.com>
2018-11-30 20:38:54 +0800

    增加了主函数

--- src/com/newer/Demo/App.java
+++ src/com/newer/Demo/App.java
@@ -1,5 +1,14 @@
 package com.newer.Demo;
-
+/**
+ * 
+ * ”¶”√¿‡
+ * @author 22279
+ *
+ */
 public class App {
-
+	public static void main(String[] args) {
+		
+		System.out.println("Demo");
+		
+	}
 }
